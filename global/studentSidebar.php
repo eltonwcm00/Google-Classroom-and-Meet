fa3b8e8654042659476f5f5e56f94831ab09300b
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-10 01:06:31 +0800

    global sidebar - done

---
+++ global/studentSidebar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/globalSidebar.css" />
+        
+    </head>
+    <body>
+    <!-- <?php include 'studentNav.php';?> -->
+        <div class="sidenav">
+            <a href="#"><p>POST ANNOUNCEMENT</p></a>
+            <a href="#"><p>VIEW ANNOUNCEMENT</p></a>
+            <a href="#"><p>SEND CHAT MESSAGE</p></a>
+            <a href="#"><p>SUBMIT ASSIGNMENT</p></a>
+            <a href="#"><p>SYNCHRONIZE CALENDAR</p></a>
+            <a href="#"><p>JOIN MEET</p></a>
+        </div>
+        <!-- <div class="main">
+            <h2>Sidenav Example</h2>
+            <p>This sidenav is always shown.</p>
+        </div> -->
+    </body>
+</html> 
---
+++ global/teacherSidebar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/globalSidebar.css" />
+        
+    </head>
+    <body>
+    <!-- <?php include 'teacherNav.php';?> -->
+        <div class="sidenav">
+            <a href="#"><p>POST ANNOUNCEMENT</p></a>
+            <a href="#"><p>VIEW ANNOUNCEMENT</p></a>
+            <a href="#"><p>SEND CHAT MESSAGE</p></a>
+            <a href="#"><p>CREATE CLASSWORK SUBMISSON</p></a>
+            <a href="#"><p>VIEW ASSIGNMENT LIST</p></a>
+            <a href="#"><p>CREATE MEET</p></a>
+            <a href="#"><p>SYNCHRONIZE CALENDAR</p></a>
+            <a href="#"><p>CREATE MEET LINKAGE</p></a>
+            <a href="#"><p>JOIN MEET</p></a>
+        </div>
+        <!-- <div class="main">
+            <h2>Sidenav Example</h2>
+            <p>This sidenav is always shown.</p>
+        </div> -->
+    </body>
+</html> 
