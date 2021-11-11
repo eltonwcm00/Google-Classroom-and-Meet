b130f1e6d181fb226ef04484f711ef91a08546af
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-11 23:43:35 +0800

    teacher view classroom main - done

---
+++ teacher/viewAnnoucement.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--THIS IS BOOTSTRAP CSS PART START-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/teacher/viewAnnoucement.css" /> 
+    </head>
+    <body style="background-color: black;">
+        <?php include '../shared/teacherNav.php';?>
+        <div class="wrapper container-xxl">
+            <?php include '../shared/teacherSidebar.php';?>
+            <div class="wrapper_sub container-xl">.</div>
+        </div>
+    </body>
+</html>
