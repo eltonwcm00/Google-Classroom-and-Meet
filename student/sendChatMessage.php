3cda28e9277bd51d8d206a8495db07a6e4b4bfcd
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-12 17:46:46 +0800

    student send chat message - 50%

---
+++ student/sendChatMessage.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/student/submitAssignment.css" /> 
+    </head>
+    <body style="background-color: black;">
+    <div style=""><?php include '../shared/studentNav.php';?>
+        <div class="wrapper container-xxl">
+        <?php include '../shared/studentSidebar.php';?>
+            <div class="row">
+                <div class="col col-lg-2" style="margin-left: -3%;">
+                    .
+                </div>
+                <div class="col">
+                    <div class="wrapper_sub container-xl">
+                        <div class="wrapper_sub_first" style="background-color: #FFD801;">
+                            <h5>TSE2101 - SOFTWARE ENG. FUND <br/>TC3V</h5>
+                        </div>
+                        <div class="wrapper_sub_second wrapper_sub_second_pos">
+                            <!-- <img id="speaker" src="../pic/speaker.png" />  -->
+                            <span id="speaker_title" style="font-size: 30px;">Chat Box</span>
+                            <br /> 
+                            <div class="row row_btwn">
+                                <div class="col">
+                                    <div class="annoucement_details">
+                                        <div class="row">
+                                            <div class="col-4 col-md-1">
+                                                <img style="border-radius: 50%; height: 49px; width: 49px;" src="../pic/teacherA.png" /> 
+                                            </div>
+                                            <div class="col">
+                                                <span id="speaker_title">Madam Jung Ho-yeon</span>
+                                            </div>
+                                        </div>
+                                        <div class="row row_pos">
+                                            <div class="col-4 col-md-1">
+                                            </div>
+                                            <div class="col">
+                                                <small id="date">June 23</small>
+                                            </div>
+                                        </div>
+                                        <br/>
+                                        <!-- <div class="row row_pos">
+                                            <div class="col-4 col-md-1">          
+                                            </div>
+                                            <div class="col" style="">
+                                                <small>The Meet link for tonight's Quiz 1</small>
+                                                <br/><a href="#test" class="testing">https://meet.google.com/yty-mazs-qfh</a>
+                                                <br/><br/>
+                                                <small>Quiz 1</small><br/>
+                                                <small>Time: 8pm (15 min)</small><br/>
+                                                <small>Topic: Lecture & Lab 01-04</small><br/>
+                                                <small>Questions 10</small><br/>   
+                                                <small>Format: MCQ, checkboxes, fill in the blank</small><br/>       
+                                            </div>
+                                        </div> -->
+                                    </div>
+                                </div>
+                                <div class="col">   
+                                </div>
+                            </div>
+
+
+                            <div class="row row_btwn">
+                                <div class="col">
+                                    <div class="annoucement_details">
+                                        <div class="row">
+                                            <div class="col-4 col-md-1">
+                                                <img style="border-radius: 50%; height: 49px; width: 49px;" src="../pic/teacherA.png" /> 
+                                            </div>
+                                            <div class="col">
+                                                <span id="speaker_title">Madam Jung Ho-yeon</span>
+                                            </div>
+                                        </div>
+                                        <div class="row row_pos">
+                                            <div class="col-4 col-md-1">
+                                            </div>
+                                            <div class="col">
+                                                <small id="date">June 19</small>
+                                            </div>
+                                        </div>
+                                        <br/>
+                                        <!-- <div class="row row_pos">
+                                            <div class="col-4 col-md-1">          
+                                            </div>
+                                            <div class="col" style="">
+                                                <small>The Meet link for tonight's Quiz 1</small>
+                                                <br/><a href="#test" class="testing">https://meet.google.com/yty-mazs-qfh</a>
+                                                <br/><br/>
+                                                <small>Tutorial 8</small><br/>
+                                                <small>Questions 10</small><br/>   
+                                                <small>Format: MCQ, checkboxes, fill in the blank</small><br/>       
+                                            </div>
+                                        </div> -->
+                                    </div>
+                                </div>
+                                <div class="col">   
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
+        </div>
+    </body>
+</html>
