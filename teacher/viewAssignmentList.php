dc6a6ac033869b8323962b8afb84a756c30ed382
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-17 01:31:46 +0800

    teacher view assignment list - done

---
+++ teacher/viewAssignmentList.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/teacher/viewAnnoucements.css" /> 
+        <link rel="stylesheet" href="../css/teacher/viewAssignmentList.css" /> 
+    </head>
+    <body style="background-color: black;">
+    <div style=""><?php include '../shared/teacherNav.php';?>
+        <div class="wrapper container-xxl">
+        <?php include '../shared/teacherSidebar.php';?>
+            <div class="row">
+                <div class="col col-lg-2" style="margin-left: -3%;">
+                    .
+                </div>
+                <div class="col">
+                    <div class="wrapper_sub container-xl">
+                        <div class="wrapper_sub_first" style="background-color: #FFD801;">
+                            <h5>TSE2101 - SOFTWARE ENG. FUND <br/>TC3V</h5>
+                        </div>
+                        <div class="wrapper_sub_second wrapper_sub_second_pos">
+                            <h2>Assignment List</h2>
+
+                            <div class="row" style="margin-top: 5%; margin-left: .3%;">
+                                <div class="column1">
+                                    <h4>Topics</h4>
+                                    <h5 id="left">Assignments</h5>
+                                    <h5 id="left">Tutorials</h5>
+                                    <h5 id="left">General Instructions</h5>
+                                </div>
+
+                                <div class="column2" style="margin-left: 30%;">
+                                    <h3>Assignments</h3>
+                                    <div2 class="row">
+                                        <div2 class="column3">
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Software Design : Interface Design</label>
+                                            <br>
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Software Design : Architecture Design</label>
+                                        </div2>
+                                    </div2>
+                                    <br><br>
+                                    <h3>Tutorials</h3>
+                                    <div2 class="row">
+                                        <div2 class="column3">
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Tutorial 1: Introduction To Software Engineering</label>
+                                            <br>
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Tutorial 2: Software Process</label>
+                                        </div2>
+                                    </div2>
+                                    <br><br>
+                                    <h3>General Instructions</h3>
+                                    <div2 class="row">
+                                        <div2 class="column3">
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Software Design : Interface Design</label>
+                                            <br>
+                                            <img src="../pic/to-do.jpg" alt="TC ass list" style="width: 49px; height: 49px;"> <label><br>Software Design : Architecture Design</label>
+                                        </div2>
+                                    </div2>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            </div>
+        </div>
+    </body>
+</html>
