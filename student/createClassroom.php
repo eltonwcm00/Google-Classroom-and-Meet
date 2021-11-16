ea640e35744902f311008e7bce6c4224cacfc08f
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-16 22:03:27 +0800

    student create classroom - done

--- student/createClassroom.php
+++ student/createClassroom.php
@@ -4,11 +4,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/teacher/createClassroom.css" /> 
+        <style>
+            .dropdown {
+                margin-top: 0 !important;
+            }
+        </style>
     </head>
     <body style="background-color: black;">
-    <div style=""><?php include '../shared/teacherNav.php';?>
+    <div style=""><?php include '../shared/test.php';?>
         <div class="wrapper container-xxl">
-        <?php include '../shared/teacherSidebar.php';?>
+        <?php include '../shared/studentSidebar.php';?>
             <div class="row">
                 <div class="col col-lg-2" style="margin-left: -3%;">
                     .
