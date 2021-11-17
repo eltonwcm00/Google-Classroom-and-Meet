553986528a40d170ebbdc54f7ee9f2196fce3891
eltonwcm00 <eltonwcm00@gmail.com>
2021-11-18 01:25:22 +0800

    admin login backend -done

--- admin/index.php
+++ admin/index.php
@@ -27,18 +27,62 @@
                     <div class="row_form">
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address:</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" class="form-control" name="emailInput" id="exampleInputEmail1" aria-describedby="emailHelp" required>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password:</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <input type="password" class="form-control" name="passwordInput" id="exampleInputPassword1" required>
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-warning btn_placement">login</button>
+                            <button type="submit" name="loginbtn" class="btn btn-warning btn_placement">login</button>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
     </body>
+    <?php 
+         require('../db.php');
+
+         echo'<script type="text/JavaScript"> 
+                    alert("LOGIN DETAILS FOR ADMIN\n--------------------------------\nEmail : admin@gmail.com\nPassword : 123");
+                 </script>';
+
+                //  mysqli_query($con,"INSERT INTO admin(Admin_Email_Address, Admin_Password) VALUES ('$email', '$password')");
+
+         if(isset($_POST["loginbtn"])) {
+                
+                $username = 'admin';
+                $password = '123';
+                $email = 'admin@gmail.com';
+
+                $varEmail = $_POST["emailInput"];
+                $varPass = $_POST["passwordInput"];
+
+                $queryRead = mysqli_query($con,"SELECT Admin_Email_Address, Admin_Password FROM admin WHERE Admin_Email_Address = '$email'");
+                $numrows = mysqli_num_rows($queryRead);
+
+                if(!empty($varEmail) && !empty($varPass)) { 
+                      if($numrows != 0) {
+                          while($row = mysqli_fetch_assoc($queryRead)) {
+                              $db_email_login = $row['Admin_Email_Address'];
+                              $db_password_login = $row['Admin_Password'];
+                          }
+                          if($varEmail == $db_email_login &&  $varPass == $db_password_login) {
+                              session_start();
+                              $_SESSION["Admin_Email_Address"] = $varEmail;
+                              header('location:homepage.php');
+                                      
+                          } else if($varEmail != $db_email_login ||  $varPass != $db_password_login) {  
+                              echo "<script> alert('Incorrect credentials, please try again'); </script>";
+                          } 
+                      } else {
+                          $queryInsert = mysqli_query($con,"INSERT INTO admin(Admin_Email_Address, Admin_Password) VALUES ('$email', '$password')");
+                          header('location:index.php');
+                      }
+                } else {
+                    echo "<script> alert('Please don't leave the input blank!'); </script>";
+                }
+        }
+    ?>
 </html>
